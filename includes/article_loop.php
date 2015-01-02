d1545eae44d052cfcc6774a0c9bba36c8c29b998
Friso van Dijk <mail@frisovandijk.com>
2015-01-02 18:22:19 +0100

    Fixed wrong link

--- includes/article_loop.php
+++ includes/article_loop.php
@@ -4,5 +4,5 @@
     <small><?php echo "Posted " . article_date() . " in <a href='". category_url() ."'>" . category_title() . "</a>"; ?></small>
   </header>
   <?php echo article_markdown(); ?>
-  <p class="back-to-blog"><br/><a href="<?php echo base_url(); ?>blog">Back to blog.</a></p>
+  <p class="back-to-blog"><br/><a href="javascript:history.back()">Back to blog.</a></p>
 </article>
