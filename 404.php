c2928ff79b626a88cc6deb0800ddac73a6fbfa4b
Friso van Dijk <mail@frisovandijk.com>
2015-01-04 17:20:07 +0100

    Added image creator credentials
    Allowed to use the image by author. Link to author deviantart page
    provided.

--- 404.php
+++ 404.php
@@ -6,9 +6,10 @@
       <header>
         <h1>Well, you've done it. It's broken!</h1>
       </header>
-      <p>
+      <figure>
         <img src="http://fc01.deviantart.net/fs71/f/2013/348/3/d/nope__animated_gif_by_saturn_kitty-d6xx1k2.gif" alt="404 gif">
-      </p>
+        <figcaption><a href="http://saturn-kitty.deviantart.com/">Image by Page Branson</a></figcaption>
+      </figure>
 
       <p>Maybe it was "just an accident". In that case, you can always <a href="javascript:history.go(-1)">go back</a></p>
       <p>If we accidentally removed a link, then we're truly sorry.</p>
